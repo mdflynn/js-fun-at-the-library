554f14619066a1305d8b991f6c447c4d8bfa7cb8
mdflynn <mdflynn34@outlook.com>
2020-08-20 08:58:12 -0600

    Add book solutions

--- src/book.js
+++ src/book.js
@@ -1,12 +1,45 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return `The ${title}`
+}
+
+function buildMainCharacter(name, age, pronouns) {
+  return {
+    name: name,
+    age: age,
+    pronouns: pronouns
+  };
+}
+
+function saveReview(review, array) {
+  if (!array.includes(review)) {
+    array.push(review);
+  };
+}
+
+function calculatePageCount(title) {
+  return title.length * 20;
+}
+
+function writeBook(title, mainCharacter, genre) {
+  return {
+    title: title,
+    mainCharacter: mainCharacter,
+    pageCount: calculatePageCount(title),
+    genre: genre,
+  };
+}
+
+function editBook(book) {
+  book.pageCount = book.pageCount * 0.75;
+  return book.pageCount
+  //why doesn't returning book.pageCount * 0.75 work?
 }
 
 module.exports = {
   createTitle: createTitle,
-  // buildMainCharacter: buildMainCharacter,
-  // saveReview: saveReview,
-  // calculatePageCount: calculatePageCount,
-  // writeBook: writeBook,
-  // editBook: editBook
+  buildMainCharacter: buildMainCharacter,
+  saveReview: saveReview,
+  calculatePageCount: calculatePageCount,
+  writeBook: writeBook,
+  editBook: editBook
 }
