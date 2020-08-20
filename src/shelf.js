afc796e89f860443fe65269ef74d2b12456d5ef8
mdflynn <mdflynn34@outlook.com>
2020-08-20 08:59:15 -0600

    Add shelf solutions

--- src/shelf.js
+++ src/shelf.js
@@ -1,8 +1,36 @@
+function shelfBook(bookObject, shelf) {
+  if (shelf.length < 3) {
+    shelf.unshift(bookObject); //passes test but doesn't sort
+  }
+};
 
+function unshelfBook(bookTitle, shelf) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === bookTitle) {
+      shelf.splice(i, 1)
+    }
+  }
+};
+
+function listTitles(shelf) {
+  var titleHolder = [];
+  for (var i = 0; i < shelf.length; i++) {
+    titleHolder.push(shelf[i].title)
+  } var title = titleHolder.join(', ');
+  return title;
+};
+
+function searchShelf(shelf, bookTitle) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === bookTitle) {
+      return true;
+    }
+  } return false;
+};
 
 module.exports = {
-  // shelfBook: shelfBook,
-  // unshelfBook: unshelfBook,
-  // listTitles: listTitles,
-  // searchShelf: searchShelf
+  shelfBook: shelfBook,
+  unshelfBook: unshelfBook,
+  listTitles: listTitles,
+  searchShelf: searchShelf
 };
